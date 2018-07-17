368aa245d4d40e3b46ac5492d63df4a52bb1ba1f
GBTC406017UR <GBTC406017UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-17 12:48:28 -0400

    Initial commit

---
+++ src/main/java/com/example/lifebook/LifebookApplication.java
@@ -0,0 +1,12 @@
+package com.example.lifebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifebookApplication.class, args);
+	}
+}
